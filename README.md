c1476f7b02bf5f9bb6f68e4bab20e1e42f4604e3
Nathan Fischer <nfischer921@gmail.com>
2021-02-09 15:29:18 -0800

    sauce

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-sauce-git
+
+Embed snippets from source code on GitHub using [highlight.js](https://highlightjs.org) and [web components](https://developer.mozilla.org/en-US/docs/Web/Web_Components).
+All client side, all free and open source.
+
+## Importing
+
+```html
+<script type='module' src="https://kag0.github.io/sauce/sauce.js"></script>
+```
+
+<script type='module' src="https://kag0.github.io/sauce/sauce.js">
+<h1> [If you're reading this, click here](https://kag0.github.io/sauce)</h1>
+</script>
+
+## Embedding a file
+
+<sauce-code
+  repo='kag0/sauce' 
+  file='example.html'
+  lines='12:15'
+></sauce-code>
+
+## Overriding the language detection
+
+<sauce-code
+  repo='kag0/sauce' 
+  file='example.html'
+  lines='17:21'
+></sauce-code>
+
+## Embedding a snippet from a file
+
+<sauce-code
+  repo='kag0/sauce' 
+  file='example.html'
+  lines='23:27'
+></sauce-code>
+
+## Changing the theme
+
+You can find a list of themes [here](https://cdnjs.com/libraries/highlight.js). 
+The theme name is between the last `/` and the `.min.css`.  
+Previews of themes are [here](https://highlightjs.org/static/demo/).
+
+<sauce-code
+  lang='html'
+  repo='kag0/sauce' 
+  file='example.html'
+  theme='monokai-sublime'
+  lines='29:33'
+></sauce-code>
---
+++ sauce.js
@@ -0,0 +1,48 @@
+import hljs from 'https://cdn.skypack.dev/highlight.js@10.6.0';
+
+class Sauce extends HTMLElement {
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		let repo = this.getAttribute('repo');
+		let file = this.getAttribute('file');
+		let branch = this.hasAttribute('branch') ? this.getAttribute('branch') : 'master';
+		let lines = this.hasAttribute('lines') ? this.getAttribute('lines').split(':') : [1, Number.MAX_VALUE];
+		let startLine = Math.max(lines[0] -1, 0);
+		let endLine = lines[1];
+		let lang = this.getAttribute('lang');
+
+		let pre = document.createElement('pre');
+		pre.style.marginLeft = '1em';
+		let code = document.createElement('code');
+		pre.appendChild(code);
+		if (lang) {
+			code.classList.add('lang-'+lang);
+		}
+			
+		let theme = this.hasAttribute('theme') ? this.getAttribute('theme') : 'tomorrow';
+		let style = document.createElement('link');
+		style.setAttribute('rel', 'stylesheet');
+		style.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.6.0/styles/' + theme + '.min.css');
+
+		let srcDiv = document.createElement('div');
+		let srcLink = document.createElement('a');
+		srcDiv.appendChild(srcLink);
+		srcLink.setAttribute('href', 'https://github.com/' + repo + '/blob/' + branch + '/' + file + '#L' + startLine + '-L' + endLine);
+		srcLink.innerText = file;
+		srcDiv.style.marginTop = '1em';
+		srcDiv.style.marginBottom = '1em';
+		this.shadowRoot.append(style, pre, srcDiv);
+
+		fetch('https://raw.githubusercontent.com/' + repo + '/' + branch + '/' + file)
+			.then(response => response.text())
+			.then(src => src.split('\n').slice(startLine, endLine).join('\n'))
+			.then(src => code.innerText = src)
+			.then(_ => hljs.highlightBlock(code));
+	}
+}
+
+customElements.define('sauce-code', Sauce);
